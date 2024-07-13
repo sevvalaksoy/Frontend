3c824b2e91e96b96a9cca8087454389ffe9bb7b9
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-07-13 15:43:24 +0300

    sayfa

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -9,13 +9,13 @@ export default function ProjectsPage () {
     const theme = useSelector(myStore=>myStore.mode);
     
     return (
-        <>
-            <div className={`flex flex-col gap-20 py-12 px-20 w-full ${!theme ? 'bg-d-body' : ''}`} > 
+        <div>
+            <div className={`flex flex-col lg:gap-20 py-12 lg:px-20 md:px-10 sm:gap-12 sm:px-8 xs:gap-8 xs:px-4 xs:py-8 ${!theme ? 'bg-d-body' : 'bg-white'}`} > 
                 <Mode/>
                 <Header/>
                 <Projects/>
             </div>
             <Footer/> 
-        </>
+        </div>
     )
 }
