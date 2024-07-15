c33555f9885821ccebbe6b8c4b64432ed72bb541
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-07-15 23:47:40 +0300

    rtl

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import {render} from "@testing-library/react";
+import { test } from '@jest/globals'
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import '@testing-library/jest-dom';
 
 
 test("uygulama başarı ile başlatıldı", ()=> {
